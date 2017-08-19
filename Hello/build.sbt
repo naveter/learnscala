ee1dcdaa8ad279d5288e46bc3ea23a78679593e1
igulevskiy <igulevskiy@moysklad.ru>
2017-08-20 00:34:09 +0300

    test2

---
+++ Hello/build.sbt
@@ -0,0 +1,8 @@
+name := "Hello"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+
+        
---
+++ Hello/src/main/scala/example/Main.scala
@@ -0,0 +1,9 @@
+package example
+
+/**
+  * Created by bass on 20.08.17.
+  */
+object Main extends App {
+  val ages = Seq(42, 75, 29, 64, 76)
+  println(s"The oldest person is ${ages.max}")
+}
