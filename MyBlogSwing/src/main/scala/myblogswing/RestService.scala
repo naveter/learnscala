136d746e275b773263f7b11ea2a5591d59cb1a58
bass <naveter@gmail.com>
2018-01-03 14:40:58 +0300

    MainBlogSwing RestClient p.1

--- MyBlogSwing/src/main/scala/myblogswing/RestService.scala
+++ MyBlogSwing/src/main/scala/myblogswing/RestService.scala
@@ -1,11 +1,14 @@
 package myblogswing
 
 import org.apache.http.impl.client.HttpClients
+
 import scala.util.control.Breaks._
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder
 import java.io.BufferedReader
 import java.io.InputStreamReader
+
+import scala.swing.Dialog
 
 class RestService {
 
@@ -13,46 +16,20 @@ class RestService {
 
   def getRestContent(url:String): String = {
     val rurl = serverUrl + url
-
-    val httpclient = HttpClients.createDefault
-    val httpGet = new HttpGet(rurl)
-    val response1 = httpclient.execute(httpGet)
-
-    var respond = ""
-    try {
-      val rd = new BufferedReader(new InputStreamReader(response1.getEntity.getContent))
-
-      var line = ""
-      while ( {
-        (line = rd.readLine) != null
-      }) respond += line
-
-    } finally response1.close
-
-    respond
-  }
-
-  def getRest(): String = {
-
-    val url = "http://localhost:8080/rest/categories"
-
     val client = HttpClientBuilder.create.build
-    val request = new HttpGet(url)
-
-    // add request header
-    request.addHeader("User-Agent", "Mozilla")
+    val request = new HttpGet(rurl)
     val response = client.execute(request)
 
-    System.out.println("Response Code : " + response.getStatusLine.getStatusCode)
+    if(200 != response.getStatusLine.getStatusCode){
+      Dialog.showMessage(null, "Network not found", title="Error")
+      return ""
+    }
 
     val rd = new BufferedReader(new InputStreamReader(response.getEntity.getContent))
-
     val result = new StringBuffer
     var line = ""
     breakable {
       while ((line = rd.readLine()) != null) {
-
-
         if (line == null) break()
 
         result.append(line)
@@ -62,9 +39,7 @@ class RestService {
   }
 
   def getCategories():Array[Category] = {
-//    val cont = getRestContent("categories")
-
-    val cont = getRest()
+    val cont = getRestContent("categories")
 
     Array(new Category())
   }
