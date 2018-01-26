a8adeac779edfc7d81e7e6a7d0151d37a6ebdd9a
bass <naveter@gmail.com>
2018-01-26 17:03:32 +0300

    learnscala cookbook WebServices

--- Hello/src/main/scala/cookbook/WebServices.scala
+++ Hello/src/main/scala/cookbook/WebServices.scala
@@ -2,7 +2,21 @@ package cookbook
 
 object WebServices extends App {
 
-
+  //Recipe 15.1, How to create a JSON string from a Scala object
+  //Recipe 15.2, How to create a JSON String from Scala classes that have collections
+  //Recipe 15.3, How to create a simple Scala object from a JSON String
+  //Recipe 15.4, How to parse JSON data into an array of Scala objects
+  //Recipe 15.5, How to create simple web services with Scalatra
+  //Recipe 15.6, How to replace XML servlet mappings with Scalatra mounts
+  //Recipe 15.7, How to access Scalatra web service GET parameters
+  //Recipe 15.8, How to access POST request data with Scalatra
+  //Recipe 15.9, How to write a simple HTTP GET request client in Scala
+  //Recipe 15.10, How to create a Twitter client in Scala
+  //Recipe 15.11, How to send JSON POST data to a REST url
+  //Recipe 15.12, How to access HTTP response headers after making an HTTP request with Apache HttpClient
+  //Recipe 15.13, How to set HTTP headers when sending a web service request
+  //Recipe 15.14, How to write a GET request web service with the Play Framework
+  //Recipe 15.15, How to write a Play Framework POST request web service
 
 
 
